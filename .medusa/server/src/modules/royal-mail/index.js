0ac6e6f4d91786b3c79b597d8ac2dca4962cc458
Baam25 <samualexekko@gmail.com>
2026-03-06 16:17:47 -0400

    export module to load polling job

--- .medusa/server/src/modules/royal-mail/index.js
+++ .medusa/server/src/modules/royal-mail/index.js
@@ -1,8 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.provider = exports.ROYAL_MAIL_MODULE = void 0;
 const utils_1 = require("@medusajs/framework/utils");
 const service_1 = require("./service");
-exports.default = (0, utils_1.ModuleProvider)(utils_1.Modules.FULFILLMENT, {
+exports.ROYAL_MAIL_MODULE = "royal-mail";
+// Needed for plugins array registration — enables job/workflow scanning
+exports.default = (0, utils_1.Module)(exports.ROYAL_MAIL_MODULE, {
+    service: service_1.RoyalMailProviderService,
+});
+// Needed for fulfillment provider registration
+exports.provider = (0, utils_1.ModuleProvider)(utils_1.Modules.FULFILLMENT, {
     services: [service_1.RoyalMailProviderService],
 });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi9zcmMvbW9kdWxlcy9yb3lhbC1tYWlsL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEscURBQW1FO0FBQ25FLHVDQUFvRDtBQUVwRCxrQkFBZSxJQUFBLHNCQUFjLEVBQUMsZUFBTyxDQUFDLFdBQVcsRUFBRTtJQUMvQyxRQUFRLEVBQUUsQ0FBQyxrQ0FBd0IsQ0FBQztDQUN2QyxDQUFDLENBQUEifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi8uLi8uLi8uLi9zcmMvbW9kdWxlcy9yb3lhbC1tYWlsL2luZGV4LnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7OztBQUFBLHFEQUEyRTtBQUMzRSx1Q0FBb0Q7QUFFdkMsUUFBQSxpQkFBaUIsR0FBRyxZQUFZLENBQUE7QUFFN0Msd0VBQXdFO0FBQ3hFLGtCQUFlLElBQUEsY0FBTSxFQUFDLHlCQUFpQixFQUFFO0lBQ3JDLE9BQU8sRUFBRSxrQ0FBd0I7Q0FDcEMsQ0FBQyxDQUFBO0FBRUYsK0NBQStDO0FBQ2xDLFFBQUEsUUFBUSxHQUFHLElBQUEsc0JBQWMsRUFBQyxlQUFPLENBQUMsV0FBVyxFQUFFO0lBQ3hELFFBQVEsRUFBRSxDQUFDLGtDQUF3QixDQUFDO0NBQ3ZDLENBQUMsQ0FBQSJ9
--- src/modules/royal-mail/index.ts
+++ src/modules/royal-mail/index.ts
@@ -1,6 +1,14 @@
-import { ModuleProvider, Modules } from "@medusajs/framework/utils"
+import { ModuleProvider, Modules, Module } from "@medusajs/framework/utils"
 import { RoyalMailProviderService } from "./service"
 
-export default ModuleProvider(Modules.FULFILLMENT, {
+export const ROYAL_MAIL_MODULE = "royal-mail"
+
+// Needed for plugins array registration — enables job/workflow scanning
+export default Module(ROYAL_MAIL_MODULE, {
+    service: RoyalMailProviderService,
+})
+
+// Needed for fulfillment provider registration
+export const provider = ModuleProvider(Modules.FULFILLMENT, {
     services: [RoyalMailProviderService],
 })
